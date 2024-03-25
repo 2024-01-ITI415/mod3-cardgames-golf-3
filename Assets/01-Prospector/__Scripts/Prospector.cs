a5475d6fdab44693e4549176b7dc58351e21d8d8
s-conroy <sierraconroy1@gmail.com>
2024-03-25 19:16:14 -0400

    layout

---
+++ Assets/01-Prospector/__Scripts/Prospector.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Prospector : MonoBehaviour {
+
+	static public Prospector 	S;
+
+	[Header("Set in Inspector")]
+	public TextAsset			deckXML;
+        public TextAsset                        layoutXML;
+
+
+	[Header("Set Dynamically")]
+	public Deck					deck;
+        public Layout                                   layout;
+
+	void Awake(){
+		S = this;
+	}
+
+	void Start() {
+		deck = GetComponent<Deck> ();
+		deck.InitDeck (deckXML.text);
+                Deck.Shuffle(ref deck.cards);
+	
+
+        layout = GetComponent<Layout>();
+        layout.ReadLayout(layoutXML.text);
+
+    }
+}
